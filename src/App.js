764c13128ff0466525a2e02188cfb6f14897090c
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-13 18:34:10 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+// store - stores data, think of state
+// reducer - function that used to update store
+// two arguments - state, action
+// state - old state / state before update
+// action - what happened / what update
+// 
+
+import  {createStore} from 'redux';
+
+//reducer
+function reducer(){
+  console.log('shake and bake')
+}
+
+const store = createStore(() => {});
+
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
