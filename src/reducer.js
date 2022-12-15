2384ec480904756fed1d9f7fb2fafddee0e8c8b9
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-15 10:20:32 +0300

    REMOVE item

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,20 @@
-import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 //reducer
 function reducer(state, action){
   if(action.type === CLEAR_CART){
     return{...state, cart:[]};
+  }
+  if(action.type === DECREASE){
+    console.log("you decreased amount");
+  }
+  if(action.type === INCREASE){
+    console.log("you increase amount");
+  }
+  if(action.type === REMOVE){
+    return{
+      cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)
+    };
   }
     return state;
   }
