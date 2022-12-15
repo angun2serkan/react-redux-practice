bcd99d53cdae7aae4fe2dc7287414bcf982136aa
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-15 10:59:12 +0300

    define decrease and increase functions in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -6,13 +6,32 @@ function reducer(state, action){
     return{...state, cart:[]};
   }
   if(action.type === DECREASE){
-    console.log("you decreased amount");
+    let tempCart = [];
+    if(action.payload.amount === 1){
+      tempCart = state.cart.filter(cartItem => cartItem.id !== action.payload.id);
+    }else{
+      tempCart = state.cart.map(cartItem => {
+        if(cartItem.id === action.payload.id){
+          cartItem = {...cartItem, amount: cartItem.amount - 1};
+        }
+        return cartItem;
+      });
+    }
+    return {...state, cart: tempCart};
   }
+
   if(action.type === INCREASE){
-    console.log("you increase amount");
+    let tempCart = state.cart.map((cartItem) => {
+      if(cartItem.id === action.payload.id){
+        cartItem = {...cartItem, amount: cartItem.amount + 1};
+      }
+      return cartItem;
+    })
+    return {...state, cart:tempCart}
   }
   if(action.type === REMOVE){
     return{
+      ...state,
       cart: state.cart.filter(cartItem => cartItem.id !== action.payload.id)
     };
   }
